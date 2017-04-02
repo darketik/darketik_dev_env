8e5464436583dea2e751086d686a5399504e44bb
Darketik <darketik@gmail.com>
2017-04-02 23:48:11 +0200

    Continue adding new dev env install cmds

--- clone_code.sh
+++ clone_code.sh
@@ -3,15 +3,69 @@
 cd $HOME
 mkdir opt
 
+## Install main dev env repo
 cd $HOME/opt
 git clone https://github.com/darketik/darketik_dev_env.git dev_env_scripts
-cd dev_env_scripts
-cd chmod +x *.sh
+pushd dev_env_scripts
+chmod +x *.sh
+popd
 
 ## $HOME config scripts
+# .bachrc
+# *.bachrc : mentor, xilinx, smartdv, ...
+# .bash_aliases
+# .profile
+# .gitconfig
+# .gitignore
+# .vimrc
+# .emacs
+# .vim folder
+pushd $HOME/opt/home_config_scripts
+install -v -D .bachrc $HOME
+install -v -D .bash_aliases $HOME
+install -v -D .profile $HOME
+install -v -D .gitconfig $HOME
+install -v -D .gitignore $HOME
+install -v -D .vimrc $HOME
+install -v -D .emacs $HOME
+install -v -D .vim $HOME
+popd
 
 ## $HOME/opt scripts
+pushd $HOME/opt/home_config_scripts
+install -v -D opt/bin $HOME/opt
+popd
 
 ## $HOME/work/lib
+# externals
+# 	mbed - https://github.com/mbedmicro/mbed.git
+# 	newlib - git://sourceware.org/git/newlib-cygwin.git
+# libstm32f4 - https://github.com/darketik/libstm32f4.git 
+# kicad 
 
 ## $HOME/work/projects
+# template project structure
+# mfos dual power supply 12v
+# externals
+#		chibios - https://github.com/ChibiOS/ChibiOS.git
+#		dfu-util - git://gitorious.org/dfu-util/dfu-util.git
+#		papilio_projects
+#		mios32 - svn
+#		mutable_instruments
+#		owl
+# 	sonic_potions xlr - https://github.com/SonicPotions/LXR.git
+# 	stlink - https://github.com/texane/stlink.git
+#		stm32loader - https://github.com/jsnyder/stm32loader.git
+# lua
+# papilio_pro_fpga
+# rpi_2
+# soundbite
+# spice_sim
+# stm32f4discovery - https://github.com/darketik/stm32f4discovery.git
+# sw4stm32
+
+## gnupg keys
+## ssh keys
+## openvpn config
+
+## thunderbird mail account
--- install_env.sh
+++ install_env.sh
@@ -89,6 +89,57 @@ sudo apt-get install oracle-java8-set-default
 # TODO
 
 ##########################
+## Audio tools
+##########################
+## alsa
+sudo apt-get install -y alsa-base alsa-utils gstreamer1.0-alsa linux-sound-base alsa-firmware-loaders alsa-oss alsa-tools alsa-tools-gui alsamixergui alsaplayer-jack oss-compat
+sudo apt-get install -y paman pavucontrol pavumeter
+
+## jackd audio connection kit
+# https://github.com/jackaudio
+sudo apt-get install -y libjack-jackd2-0 libjack-jackd2-dev pulseaudio-module-jack jackd2 jack-tools qjackctl
+
+## PureData - pd-extended
+# https://github.com/agraef/purr-data
+cd $HOME/opt/src
+wget https://github.com/agraef/purr-data/releases/download/2.1.2/pd-l2ork-2.1.2-ubuntu_14.04-x86_64.deb
+sudo dpkg -i pd-l2ork-2.1.2-ubuntu_14.04-x86_64.deb
+
+## superCollider 
+# https://supercollider.github.io/
+sudo add-apt-repository ppa:supercollider/ppa
+sudo apt-get update
+sudo apt-get install -y supercollider
+
+## Chuck
+# http://chuck.cs.princeton.edu/
+sudo apt-get install -y libsndfile1-dev
+cd $HOME/opt/src
+wget http://chuck.cs.princeton.edu/release/files/chuck-1.3.5.2.tgz
+
+# needs qt 4.8+, alsa or jack
+wget http://audicle.cs.princeton.edu/mini/release/files/miniAudicle-1.3.5.2.tgz
+
+
+## FaustLive
+# TODO
+
+## Processing 
+# TODO
+
+## Csound / Cecilia
+# TODO
+
+## openNI (gesture tracking) / openCV (computer vision)
+# TODO
+
+## Festival - speech synthesis
+# TODO
+
+## Gnaural - binaural audio generation
+# TODO
+
+##########################
 ## Misc
 ##########################
 sudo apt-get install -y vim vim-gnome
@@ -109,8 +160,7 @@ sudo apt-get install -y gitk
 sudo apt-get install -y system-config-samba
 sudo apt-get install -y cutecom
 sudo apt-get install -y nmap
-sudo apt-get install -y lua5.1
-sudo apt-get install -y liblua5.1-0-dev
+sudo apt-get install -y subversion
 
 ## basic dev tools
 sudo dpkg --add-architecture i386
@@ -118,7 +168,11 @@ sudo apt-get update -qq
 sudo apt-get install -y build-essential autotools-dev autoconf pkg-config libusb-1.0-0 libusb-1.0-0-dev libftdi1 libftdi-dev git libc6:i386 libncurses5:i386 libstdc++6:i386 cowsay figlet language-pack-en libgtk-3-dev
 sudo locale-gen UTF-8
 
+## lua
+sudo apt-get install -y lua5.1
+sudo apt-get install -y liblua5.1-0-dev
 
+## atom editor
 cd $HOME/opt/src
 wget https://github.com/atom/atom/releases/download/v1.15.0/atom-amd64.deb 
 sudo dpkg -i atom-amd64.deb
@@ -138,41 +192,23 @@ sudo pip3 install pyelftools
 ## https://docs.gitlab.com/ee/integration/jenkins.html#configure-the-jenkins-server
 ## https://github.com/jenkinsci/gitlab-plugin#jenkins-job-configuration
 ##########################
-# TODO
 sudo apt-get install -y jenkins
 sudo apt-get install -y gitlab-ci-multi-runner
 
-## PureData - pd-extended
-# TODO
-
-## superCollider 
-# TODO
-
-## Chuck
-# TODO
-
-## FaustLive
-# TODO
-
-## Processing 
-# TODO
-
-## Csound / Cecilia
-# TODO
-
-## openNI (gesture tracking) / openCV (computer vision)
-# TODO
-
-## Festival - speech synthesis
-# TODO
-
-## Gnaural - binaural audio generation
-# TODO
-
 ## Scilab
+# http://www.scilab.org/
 # TODO
+cd $HOME/opt/src
+wget http://www.scilab.org/download/6.0.0/scilab-6.0.0.bin.linux-x86_64.tar.gz
 
 ## Octave
+# https://www.gnu.org/software/octave/
+# TODO
+cd $HOME/opt/src
+wget https://ftp.gnu.org/gnu/octave/octave-4.2.1.tar.gz
+# octave forge - https://octave.sourceforge.io/
+
+## matlab
 # TODO
 
 ##########################
@@ -180,11 +216,7 @@ sudo apt-get install -y gitlab-ci-multi-runner
 ##########################
 sudo add-apt-repository --yes ppa:js-reynaud/kicad-4
 sudo apt-get update
-sudo apt-get install -y kicad
-sudo apt-get install -y geda pcb gerbv
-sudo apt-get install -y easyspice ngspice ngspice-doc
-sudo apt-get install -y gspiceui gnucap 
-sudo apt-get install -y oregano
+sudo apt-get install -y kicad geda pcb gerbv easyspice ngspice ngspice-doc gspiceui gnucap oregano
 
 ## LTSPICE with wine
 # TODO
@@ -192,16 +224,16 @@ sudo apt-get install -y wine
 cd $HOME/opt/src
 wget http://ltspice.linear-tech.com/software/LTspiceXVII.exe
 chmod +x LTspiceXVII.exe
-./LTspiceXVII.exe
-
+sudo ./LTspiceXVII.exe
 
 ## gtkwave
-# TODO
+# http://gtkwave.sourceforge.net/
+sudo apt-get install gtkwave
 
 ##########################
 ## AVR tools
 ##########################
-sudo apt-get install -y gcc-avr binutils-avr avr-libc avrdude
+#+ sudo apt-get install -y gcc-avr binutils-avr avr-libc avrdude
 
 ##########################
 ## STM32 F4 tools
@@ -209,11 +241,12 @@ sudo apt-get install -y gcc-avr binutils-avr avr-libc avrdude
 ## sw4stm32 workbench
 # TODO
 
-## stm32CubeMX
-# TODO
+# Audio Weaver ST edition
+# TODO get from sftp darketik NAS
 
-## Openocd
-# TODO
+## stm32CubeMX
+# https://my.st.com/content/my_st_com/en/products/embedded-software/mcus-embedded-software/stm32-embedded-software/stm32cube-embedded-software/stm32cubef4.license%3d1491167185534.html
+# TODO get from sftp darketik NAS
 
 ## Gnu-gcc-arm
 cd $HOME/opt/src
@@ -223,19 +256,23 @@ sudo mv gcc-arm-none-eabi-5_4-2016q3 $HOME/opt
 cd $HOME/opt
 ln -s gcc-arm-none-eabi-5_4-2016q3 gcc-arm-none-eabi
 
-## System Workbench for STM32
-# TODO
-
-## Stm32Cube MX
-# TODO
-
-## papilio loader
-# TODO
-
 ## arduino
+# https://www.arduino.cc/en/Main/Software
 # TODO
+cd $HOME/opt/src
+wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.2-linux64.tar.xz
 
 ## teensy
+# https://www.pjrc.com/
+cd $HOME/opt/src
+wget https://www.pjrc.com/teensy/49-teensy.rules
+sudo cp 49-teensy.rules /etc/udev/rules.d/
+cd $HOME/opt/src
+wget https://www.pjrc.com/teensy/td_136/TeensyduinoInstall.linux64
+chmod +x TeensyduinoInstall.linux64
+sudo ./TeensyduinoInstall.linux64
+
+## papilio loader
 # TODO
 
 ## papilio zap ide
@@ -280,21 +317,21 @@ sudo udevadm trigger
 ##########################
 ## Xilinx Vivado 
 ##########################
-# TODO
+# TODO get from sftp darketik NAS
 
 ##########################
 ## flexlm license server deamon startup
 ##Lmgrd license service 
 ##	Added launch cmd into /etc/rc.local file
 ##########################
-# TODO
+# TODO get from sftp darketik NAS
 
 ##########################
 ## IC EDA tools
 ##########################
 # Mentor Questasim
-# TODO
+# TODO get from sftp darketik NAS
 
 # VIP SMartDV
-# TODO
+# TODO get from sftp darketik NAS
 
