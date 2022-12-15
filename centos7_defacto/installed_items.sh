7619c0896dc3d1d77e6b64d6b69c663504545290
Florent Rotenberg <florent@defactotech.com>
2022-12-15 13:57:15 +0100

    Add centos7 os env install for defacto

---
+++ centos7_defacto/installed_items.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p SCRATCH/externals
+cd SCRATCH/externals
+
+git clone https://github.com/universal-ctags/ctags.git
+
+## TODO 
+# accellera
+# ChibiOS_21.11.0
+# ctags
+# kactus2_examples
+# lowriscv_opentitan
+# lowriscv_style_guides
+# openlane
+# Practical-UVM-Step-By-Step
+# pulp_github
+# siemens
+# systemrdl_eval
