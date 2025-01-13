84c751f34aee60c7a15b16bc43cd07a9ea6ecae6
Florent Rotenberg <florent.rotenberg@pyxalis.com>
2025-01-13 11:40:59 +0100

    Add riuscv project

--- rh7_pyx/home_config_scripts/pyx_utils/set_pyx_proj.sh
+++ rh7_pyx/home_config_scripts/pyx_utils/set_pyx_proj.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-projet=$1
+project_id=$1
 user=$2
-workdir0="/isona1/${projet}/${projet}/FrontEnd"
-export PROJET=$projet
+workdir0="/isona1/${project_id}/${project_id}/FrontEnd"
+# export PROJECT_ID=$project_id
 cd ${workdir0}
 source .${user}cshrc
