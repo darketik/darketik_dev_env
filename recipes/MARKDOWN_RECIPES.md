3ce468e6f5d176765bed014f934811454e2d70ef
Florent Rotenberg <florent.rotenberg@pyxalis.com>
2025-03-04 10:09:22 +0100

    :art: build: change var names to limit conflicts

--- recipes/MARKDOWN_RECIPES.md
+++ recipes/MARKDOWN_RECIPES.md
@@ -3,3 +3,5 @@ https://www.markdownguide.org/cheat-sheet/
 
 http://csrgxtu.github.io/2015/03/20/Writing-Mathematic-Fomulars-in-Markdown/
 
+https://github.com/highlightjs/highlight.js/blob/main/SUPPORTED_LANGUAGES.md
+
--- rh7_pyx/home_config_scripts/pyx_utils/set_pyx_proj.sh
+++ rh7_pyx/home_config_scripts/pyx_utils/set_pyx_proj.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 project_id=$1
-user=$2
+user_id=$2
 workdir0="/isona1/${project_id}/${project_id}/FrontEnd"
 # export PROJECT_ID=$project_id
 cd ${workdir0}
-source .${user}cshrc
+source .${user_id}cshrc
